a1e2759a709b339bfbd87f30499ed7aab0c2ad3b
Sreelr5692 <sreelr5692@gmail.com>
2024-02-16 08:58:05 +0000

    subnet.tf cidr modified

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "subnet1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id     = aws_vpc.Myvpc.id
+  cidr_block = "10.0.3.0/24"
   tags = {
     Name = "Subnet 1"
   }
