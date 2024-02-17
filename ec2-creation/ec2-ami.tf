3569e055cc3a4b5234ab8127c2c8bc3b9b3eb800
sreejith <sreejith@devops.com>
2024-02-17 11:50:40 +0000

    ami creation with variables

---
+++ ec2-creation/ec2-ami.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "Webserver2024" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+}
+
+
---
+++ ec2-creation/variables.tf
@@ -0,0 +1,6 @@
+variable "ami" {
+  default = "ami-0e731c8a588258d0d"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
